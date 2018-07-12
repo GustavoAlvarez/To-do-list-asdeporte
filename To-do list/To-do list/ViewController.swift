73191973e948bd2a157aa1c9da170bd7bb16003d
Gustavo <gustavo.a.dev@gmail.com>
2018-07-11 21:20:54 -0500

    First commit

---
+++ To-do list/To-do list/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  To-do list
+//
+//  Created by Gustavo Alvarez on 11/07/18.
+//  Copyright © 2018 ParanoidDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Mis tareas"
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
